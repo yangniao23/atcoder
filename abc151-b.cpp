44b416e03f43c9a3844e8c6ac1ea0e8739819178
yangniao23 <yangniao23@gmail.com>
2020-08-26 23:19:19 +0900

    SS 8/26

---
+++ abc151-b.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int n, k, m;
+    int pointsum = 0;
+    int a[100];
+    cin >> n;
+    cin >> k;
+    cin >> m;
+    for(int i=0;i<n-1;++i) {
+        cin >> a[i];
+        pointsum += a[i];
+    }
+    for(int i=0;i<k+1;++i) {
+        if((pointsum+i)/n >= m ) {
+            cout << i << '\n';
+            return 0;
+        }
+    }
+    cout << -1 << '\n';
+    return 0;
+}
