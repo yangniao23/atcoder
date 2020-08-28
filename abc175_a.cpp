68d21d26fab733f7c8bb43ebb0d3dfe256ec7a4d
yangniao23 <yangniao23@gmail.com>
2020-08-28 22:38:58 +0900

    NS なんもわからん

---
+++ abc175_a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define ll long long
+inline int roundup(int a, int b) { return (((a + (b -1)) / b)); }
+using namespace std;
+
+signed main(void) {
+    string s;
+    int count = 0;
+    cin >> s;
+    for(int i=0; i<3; i++) {
+        
+    }
+    cout << count << '\n';
+    return 0;
+}
