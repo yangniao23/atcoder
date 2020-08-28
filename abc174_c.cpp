6e28964f4eebbd4a3384ade3aead35c7d0dc9f38
yangniao23 <yangniao23@gmail.com>
2020-08-28 21:43:22 +0900

    SO 00:05:20

--- abc174_c.cpp
+++ abc174_c.cpp
@@ -4,8 +4,18 @@ inline int roundup(int a, int b) { return (((a + (b -1)) / b)); }
 using namespace std;
 
 signed main(void) {
+    int a[1000001];
     int k;
-    cin >> k;
-    
-    return 0;
+    scanf("%d", &k);
+    a[1]=7%k;
+    for(int i=2;i<=k;i++){
+        a[i]=(a[i-1]*10+7)%k;
+    }
+    for(int i=1;i<=k;i++){
+        if(a[i]==0){
+            printf("%d\n", i);
+            return 0;
+        }
+    }
+    printf("-1\n");
 }
