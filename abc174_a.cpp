04211a76d19112aa75b711f703abfedec77ce597
yangniao23 <yangniao23@gmail.com>
2020-08-28 18:26:36 +0900

    SS 00:02:00

---
+++ abc174_a.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define ll long long
+inline int roundup(int a, int b) { return (((a + (b -1)) / b)); }
+using namespace std;
+
+signed main(void) {
+    int x;
+    cin >> x;
+    (x > 29)?cout << "Yes\n" : cout << "No\n";
+    return 0;
+}
