6a1806b00b83be6b32d91df1576756b1f964f07f
yangniao23 <yangniao23@gmail.com>
2020-08-28 22:56:12 +0900

    SO 00:05:00

--- abc175_b.cpp
+++ abc175_b.cpp
@@ -1,24 +1,32 @@
+//copied from https://atcoder.jp/contests/abc175/editorial/50
+
 #include <bits/stdc++.h>
 #define ll long long
+#define rep(i,n) for(int i=0;i<n;++i)
 inline int roundup(int a, int b) { return (((a + (b -1)) / b)); }
 using namespace std;
 
 signed main(void) {
-    int n, l[100], count=0;
-    
-    scanf("%d", &n);
-    for(int i=0; i<n; i++) {
-        scanf("%d", &l[i]);
-    }
-    for(int i=0; i<n-1; i++) {
-        for(int j=0; j<n-1; j++) {
-            if( (l[j] != l[j+1])) {
-                if((l[j] + l[j+1]) > l[i] && l[i] > (l[j] && l[j+1])) {
-                    count++;
+    int N;
+    cin >> N;
+    vector<int> vec(N);
+    for (int i = 0; i < N; ++i) cin >> vec[i];
+    sort(vec.begin(), vec.end());
+ 
+    int ans = 0;
+ 
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < i; ++j) {
+            for (int k = 0; k < j; ++k) {
+                if (vec[k] != vec[j] && vec[i] != vec[j] &&
+                    vec[k] + vec[j] > vec[i]) {
+                    ans++;
                 }
             }
         }
     }
-    cout << count << '\n';
+ 
+    cout << ans << endl;
+ 
     return 0;
 }
