6178338df2e0e9de08c0a7ac64500cfa8e0cff93
yangniao23 <yangniao23@gmail.com>
2020-08-28 22:44:08 +0900

    SO 00:03:00

--- abc175_a.cpp
+++ abc175_a.cpp
@@ -3,13 +3,16 @@
 inline int roundup(int a, int b) { return (((a + (b -1)) / b)); }
 using namespace std;
 
-signed main(void) {
-    string s;
-    int count = 0;
-    cin >> s;
-    for(int i=0; i<3; i++) {
-        
+signed main() {
+ int ans=0,R=0;
+  string s;
+  cin>>s;
+  for(int i=0;i<3;i++)
+    if(s[i]=='R') {
+        ans++;
+        if(R<ans) R=ans; 
     }
-    cout << count << '\n';
-    return 0;
+    else ans=0;
+  
+  cout<<R << '\n';
 }
