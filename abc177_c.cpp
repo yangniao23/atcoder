e5db20e48222ffff43763fc4924e5aad00afc798
yangniao23 <yangniao23@gmail.com>
2020-08-30 00:20:59 +0900

    SO 00:35:00

--- abc177_c.cpp
+++ abc177_c.cpp
@@ -1,22 +1,33 @@
+// copied from https://atcoder.jp/contests/abc177/submissions/16358885
+
 #include <iostream>
-#define rep(i,n) for(int i=0;i<n;++i)
+#include <vector>
 #define ll long long
-inline int roundup(int a, int b) { return (((a + (b -1)) / b)); }
 using namespace std;
 
-signed main(void) {
-    long n, A[200000];
-    long sum;
-    cin >> n;
-    for(int i=0;i<n;++i) scanf("%ld", &A[i]);
-    for(int i=0;i<n;++i) {
-        for(int j=0; j<i;++j) {
-            sum += ((A[j] % 1000000007)*(A[i] % 1000000007));
-            sum = sum % 1000000007;
-        }
-    }
-     
+int main(void) {
+	int N;
+	cin >> N;
+	int A[N];
+	ll B[N+1]; B[0] = 0;
+	int mod = 1000000007;
 
-    cout << sum << "\n";
+	for (int i = 0; i < N; i++) {
+		cin >> A[i];
+		B[i + 1] = B[i] + A[i];
+	}
+
+	long ans = 0;
+
+	//i について全探索する
+	for (int i = 0; i < N; i++) {
+		//累積和を使ってA[i+1] + ... + A[N] を求める
+		long long sum = (B[N] - B[i + 1]) % mod;
+
+		ans += A[i] * sum;
+		ans %= mod;
+	}
+
+	cout << ans << endl;
     return 0;
 }
