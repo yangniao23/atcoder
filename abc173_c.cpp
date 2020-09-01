b248b78e17d1b9f93cb6cca9342eeda6708ab8d8
yangniao23 <yangniao23@gmail.com>
2020-09-01 09:58:18 +0900

    NS 00:20:00

--- abc173_c.cpp
+++ abc173_c.cpp
@@ -5,18 +5,26 @@ inline int roundup(int a, int b) { return (((a + (b -1)) / b)); }
 using namespace std;
 
 signed main(void) {
-    int h, w, k, white=0, black=0;
-    int c[6][6];
+    int h, w, k, ans=0;
     cin >> h >> w >> k;
+    string c[h];
     rep(i, h) {
-        rep(j, w) {
-            cin >> c[i][j];
-            if(c[i][j] == '-') {
-                white++;
-            } else if(c[i][j] == '#') {
-                black++;
+        cin >> c[i];
+    }
+    rep(i, (1<<h)) {  // 2^h回ループを回す
+        rep(j, (1<<w)) { // 2^w回ループを回す
+            int cnt = 0;
+            rep(k, h) {
+                if(( i & (i << k)) != 0) continue; // i AND (iをkビット左シフトしたもの)が0でなければcontinue(0なら続行)
+                rep(l, w) {
+                    if((j & (1<<l)) != 0) continue; // j AND 2^lが0ならば続行
+                    if(c[k][l] == '#') cnt++; // c[k][l]が黒なら追加
+                }
             }
+            if(cnt == k) ans++;
         }
     }
+
+    cout << ans << '\n';
     return 0;
 }
