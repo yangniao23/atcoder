ac413b3744835117bee077f47dfe6ded9321e8dd
yangniao23 <yangniao23@gmail.com>
2020-09-13 21:00:14 +0900

    Initial commit

--- mkbranch.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-[['$1' == '']] && echo "Need branch name(contest No)." && exit 1
-
-git checkout -b abc$1
-rename NNN $1 abcNNN_*
-rm mkbranch.sh
-git add . && git commit -m "Initial commit"
