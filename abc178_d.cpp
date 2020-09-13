343f7eb614dcfa3637209258b9a6be0775baa4e7
yangniao23 <yangniao23@gmail.com>
2020-09-13 23:06:57 +0900

    NS

---
+++ abc178_d.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for(int i=0;i<n;++i)
+#define ll long long
+inline int roundup(int a, int b) { return (((a + (b -1)) / b)); }
+using namespace std;
+
+signed main(void) {
+    int s, count = 0;
+    cin >> s;
+    while(count <)
+    return 0;
+}
