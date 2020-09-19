10e3191fed867fd8be78057f45d3c504741af15e
yangniao23 <yangniao23@gmail.com>
2020-09-19 21:00:33 +0900

    a

--- abc178_d.cpp
+++ abc178_d.cpp
@@ -1,12 +1,16 @@
 #include <bits/stdc++.h>
 #define rep(i,n) for(int i=0;i<n;++i)
 #define ll long long
+const ll mod=1000000007;
 inline int roundup(int a, int b) { return (((a + (b -1)) / b)); }
+inline ll powmod(ll x, ll y) {ll res=1; for(ll i=0;i<y;i++){ res=res*x%mod; } return res;}
 using namespace std;
 
 signed main(void) {
-    int s, count = 0;
+    int s, ans = 0;
     cin >> s;
-    while(count <)
+    rep(i, s) {
+
+    }
     return 0;
 }
