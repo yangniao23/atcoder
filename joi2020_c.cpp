98404a1316953b624020a0a9aee6413323b33808
yangniao23 <yangniao23@gmail.com>
2020-10-17 18:15:11 +0900

    SS 15:00

---
+++ joi2020_c.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for(int i=0;i<n;++i)
+#define ll long long
+const ll mod=1000000007;
+inline int roundup(int a, int b) { return (((a + (b -1)) / b)); }
+inline ll powmod(ll x, ll y) {ll res=1; for(ll i=0;i<y;i++){ res=res*x%mod; } return res;}
+using namespace std;
+
+signed main(void) {
+    int n;
+    string s[100];
+    cin >> n;
+    rep(i, n) {
+        cin >> s[i];
+        transform(s[i].begin(), s[i].end(), s[i].begin(), ::tolower);
+    }
+    
+    rep(i, n) {
+        cout << s[i] << '\n';
+    }
+
+    return 0;
+}
