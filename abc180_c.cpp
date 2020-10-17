cbfa975326ef1255e6558360c83c279d822900ea
yangniao23 <yangniao23@gmail.com>
2020-10-17 21:43:03 +0900

    NS

---
+++ abc180_c.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for(int i=0;i<n;++i)
+#define ll long long
+const ll mod=1000000007;
+inline int roundup(int a, int b) { return (((a + (b -1)) / b)); }
+inline ll powmod(ll x, ll y) {ll res=1; for(ll i=0;i<y;i++){ res=res*x%mod; } return res;}
+using namespace std;
+
+vector<unsigned long long> divisor(long long n) {
+    vector<unsigned long long> ret;
+    for (unsigned long long i = 1; i * i <= n; i++) {
+        if (n % i == 0) {
+            ret.push_back(i);
+            if (i * i != n) ret.push_back(n / i);
+        }
+    }
+    sort(ret.begin(), ret.end()); // 昇順に並べる
+    return ret;
+}
+
+signed main(void) {
+    unsigned ll n;
+    cin >> n;
+    vector<unsigned long long> ans = divisor(n);
+    copy(begin(ans), end(ans), ostream_iterator<int>(cout, "\n"));
+    return 0;
+}
