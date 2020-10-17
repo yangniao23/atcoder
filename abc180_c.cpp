409a50ada59c6ea96b6e6279399df4fe6f780f0d
yangniao23 <yangniao23@gmail.com>
2020-10-17 23:58:18 +0900

    SO

--- abc180_c.cpp
+++ abc180_c.cpp
@@ -6,22 +6,17 @@ inline int roundup(int a, int b) { return (((a + (b -1)) / b)); }
 inline ll powmod(ll x, ll y) {ll res=1; for(ll i=0;i<y;i++){ res=res*x%mod; } return res;}
 using namespace std;
 
-vector<unsigned long long> divisor(long long n) {
-    vector<unsigned long long> ret;
-    for (unsigned long long i = 1; i * i <= n; i++) {
-        if (n % i == 0) {
-            ret.push_back(i);
-            if (i * i != n) ret.push_back(n / i);
-        }
-    }
-    sort(ret.begin(), ret.end()); // 昇順に並べる
-    return ret;
-}
-
 signed main(void) {
     unsigned ll n;
+    vector<ll> ans;
     cin >> n;
-    vector<unsigned long long> ans = divisor(n);
-    copy(begin(ans), end(ans), ostream_iterator<int>(cout, "\n"));
+    for(ll i=1; i*i <= n; ++i) {
+        if(n%i == 0) {
+            ans.push_back(i);
+            if(n != i*i) ans.push_back(n/i);
+        }
+    }
+    sort(ans.begin(), ans.end());
+    for(ll x: ans) cout << x << '\n';
     return 0;
 }
