2022d307f75d4dd2317607b6d9a9ffc9288bea77
yangniao23 <yangniao23@gmail.com>
2020-10-18 00:29:58 +0900

    Update: commit.sh remove when run commit.sh.

--- commit.sh
+++ commit.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
+rm commit.sh *.out
 for i in a b c d; do
-    git add *_${i}.cpp
+    git add *_${i}.cpp    
     git commit
 done
-rm commit.sh *.out
+git rm commit.sh
+git commit -m "Remove 'commit.sh'"
 git push --all origin
 git checkout master
 git checkout .
