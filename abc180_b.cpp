07adcebce954e2650c18405a7b727c2871080d5c
yangniao23 <yangniao23@gmail.com>
2020-10-17 23:22:13 +0900

    SO

--- abc180_b.cpp
+++ abc180_b.cpp
@@ -6,31 +6,19 @@ inline int roundup(int a, int b) { return (((a + (b -1)) / b)); }
 inline ll powmod(ll x, ll y) {ll res=1; for(ll i=0;i<y;i++){ res=res*x%mod; } return res;}
 using namespace std;
 typedef long double ld;
-const ld eps = 1.0e-9; // 許容される誤差
-
-ld mysqrtl(ld x)
-{
-  ld a = sqrt((double)x); // 近似値
-  do {
-    a = (a + x/a) / 2.0L;
-  } while (fabsl(x - a*a) > eps);
-  return a;
-}
 
 signed main(void) {
-    int n, x[100000], m=0, c=0, u_sq=0;
+    cout << fixed << setprecision(15);
+    ll n, x[100000], m=0, c=0, u_sq=0;
     cin >> n;
     rep(i, n) {
         cin >> x[i];
     }
     rep(i, n) {
-        if(m < abs(x[i])) c = abs(x[i]);
+        c = max(c, abs(x[i]));
         m += abs(x[i]);
         u_sq += abs(x[i])*abs(x[i]);
     }
-    ld u = mysqrtl((ld)u_sq);
-    cout << m << '\n';
-    printf("%.15Lf\n",u);
-    cout << c << '\n';
+    cout << m << '\n' << sqrt(u_sq) <<'\n' << c << '\n';
     return 0;
 }
