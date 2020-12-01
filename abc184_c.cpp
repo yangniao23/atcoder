89669b4e5dabf09b1c06523e9bfc298b818499cc
yangniao23 <yangniao23@gmail.com>
2020-12-01 16:06:34 +0900

    SS

--- abc184_c.cpp
+++ abc184_c.cpp
@@ -9,7 +9,12 @@ using namespace std;
 signed main(void) {
     ll r1, c1, r2, c2;
     cin >> r1 >> c1 >> r2 >> c2;
-
-    
+    ll r = r2 - r1;
+    ll c = c2 - c1;
+    int ans = 3;
+    if(r == 0 && c == 0) ans = 0;
+    else if(r == c || r == -c || abs(r) + abs(c) <= 3) ans = 1;
+    else if( (r1+r2+c1+c2) % 2 == 0 || abs(r) + abs(c) <= 6 || abs(r+c) <= 3 || abs(r-c) <= 3) ans = 2;
+    cout << ans << '\n';
     return 0;
 }
