f8a051c0d10f066664834288ba6104fe8d62bc4e
yangniao23 <yangniao23@gmail.com>
2021-07-22 23:58:00 +0900

    commit

---
+++ abc204_a.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for(int i=0;i<n;++i)
+#define ll long long
+const ll mod=1000000007;
+inline int roundup(int a, int b) { return (((a + (b -1)) / b)); }
+inline ll powmod(ll x, ll y) {ll res=1; for(ll i=0;i<y;i++){ res=res*x%mod; } return res;}
+using namespace std;
+
+signed main(void) {
+	int a, b, c;
+	cin >> a >> b;
+	(a == b)? cout << a : cout << 3 - a - b;
+	return 0;
+}
