673273672429244ee9451cc237bd7bdeadb7b564
Yang Niao <yangniao23@gmail.com>
2021-11-20 13:58:31 +0900

    q

---
+++ abc215_b.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for(int i=0;i<n;++i)
+#define ll long long
+const ll mod=1000000007;
+inline int roundup(int a, int b) { return (((a + (b -1)) / b)); }
+inline ll powmod(ll x, ll y) {ll res=1; for(ll i=0;i<y;i++){ res=res*x%mod; } return res;}
+using namespace std;
+
+signed main(void) {
+    ll n, k=0;
+    cin >> n;
+
+    while(n >= 2) {
+        n /= 2;
+        ++k;
+    }
+
+
+    cout << k << '\n';
+    return 0;
+}
